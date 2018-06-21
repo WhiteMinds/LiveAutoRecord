156c3c1cf69c91dceaedbdadb9537f1c32a9b88e
WhiteMind <whitemind@qq.com>
2018-06-21 16:45:51 +0800

    update

--- src/renderer/pages/LAR/index.vue
+++ src/renderer/pages/LAR/index.vue
@@ -15,36 +15,75 @@
         columns: [
           {
             title: '平台',
-            key: 'platform'
+            key: 'platform',
+            width: 100,
+            sortable: true
           },
           {
             title: '直播间',
-            key: 'room'
+            key: 'room',
+            width: 120,
+            sortable: true
           },
           {
             title: '状态',
-            key: 'status'
+            key: 'status',
+            sortable: true
           },
           {
             title: '弹幕录制',
-            key: 'danmaku'
+            key: 'danmaku',
+            width: 90,
+            render: (h, params) => {
+              return h('div', [
+                h('i-switch', {
+                  props: {
+                    value: params.row.danmaku
+                  },
+                  on: {
+                    input (val) { params.row.danmaku = val }
+                  }
+                })
+              ])
+            }
           },
           {
             title: '操作',
-            key: 'actions'
+            key: 'actions',
+            render: (h, params) => {
+              let btnTemplates = []
+              for (let i = 0; i < this.actions.length; i++) {
+                let action = this.actions[i]
+                if (action.show && !action.show(params)) continue
+                btnTemplates.push(h('Button', {
+                  props: Object.assign({ type: 'primary', size: 'small' }, action.props),
+                  style: Object.assign({ marginRight: '8px' }, action.style),
+                  on: { click: () => action.click(params) }
+                }, action.text))
+              }
+              return h('div', btnTemplates)
+            }
           }
         ],
         actions: [
           {
-            text: '终止',
+            text: '设置',
+            props: { type: 'success' },
             click: ({ row }) => {}
           },
           {
-            text: '删除',
+            text: '终止',
+            props: { type: 'warning' },
+            show: ({ row }) => row.status === '录制中',
+            click: ({ row }) => {}
+          },
+          {
+            text: '移除',
             props: { type: 'error' },
             click: () => {}
           }
-        ]
+        ],
+        liveRooms: []
       }
     },
     computed: {
@@ -56,11 +95,20 @@
           danmaku: true
         }]
         for (let i = 0; i < 5; i++) data = data.concat(data)
+        data[0] = {
+          platform: '斗鱼',
+          room: '196',
+          status: '录制中',
+          danmaku: true
+        }
         return data
       },
       tableHeight () {
         return document.documentElement.clientHeight - 80
       }
+    },
+    mounted () {
+      // this.liveRooms = ipc.remoteData
     }
   }
 </script>
