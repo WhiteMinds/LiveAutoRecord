11688edd13e2873a51c28f4a9357f6d83d4939d8
WhiteMinds <WhiteMind@qq.com>
2019-05-24 15:55:02 +0800

    [UPDATE] support auto migration after build

--- src/renderer/db/migrate.js
+++ src/renderer/db/migrate.js
@@ -1,27 +1,51 @@
 import path from 'path'
 import Umzug from 'umzug'
+import Migration from 'umzug/lib/migration'
 import log from '@/modules/log'
 import { UserDataPath } from 'const'
 
 export function run (sequelize) {
+
+  // 通过hack类Migration来支持编译后的自动迁移 (不然动态require会导致错误, 这里主要目的就是改为静态require)
+
+  const DefaultOptions = {
+    upName: 'up',
+    downName: 'down',
+    migrations: {
+      params: [sequelize.getQueryInterface(), sequelize.constructor],
+      traverseDirectories: false,
+      wrap: fun => function (args) {
+        return fun.apply(this, args || DefaultOptions.migrations.params)
+      }
+    },
+    logging: (...args) => log.debug(...args)
+  }
+
+  class MigrationHack extends Migration {
+    constructor (uniqueName, module, options = {}) {
+      super(uniqueName + '.js', Object.assign({}, DefaultOptions, options))
+      this.module = module
+    }
+
+    migration () {
+      if (typeof this.options.migrations.customResolver === 'function') {
+        return this.options.migrations.customResolver(this.path)
+      }
+
+      return this.module.default
+    }
+  }
+
   const umzug = new Umzug({
     storage: 'json',
     storageOptions: {
       path: path.join(UserDataPath, 'lar_umzug.json')
     },
-    migrations: {
-      params: [
-        sequelize.getQueryInterface(), // queryInterface
-        sequelize.constructor, // DataTypes
-        function () {
-          throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.')
-        }
-      ],
-      path: path.join(path.dirname(__filename), 'migrations'),
-      pattern: /\.js$/,
-      customResolver: migrationFile => require(`./migrations/${path.basename(migrationFile, '.js')}`).default
-    },
-    logging: (...args) => log.debug(...args)
+    migrations: [
+      new MigrationHack('00_Initial', require('./migrations/00_Initial')),
+      new MigrationHack('01_ChangeColumn_Platform', require('./migrations/01_ChangeColumn_Platform')),
+      new MigrationHack('02_ChangeColumns', require('./migrations/02_ChangeColumns'))
+    ]
   })
 
   const logUmzugEvent = eventName => {
