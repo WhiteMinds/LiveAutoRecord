41b5959583a6a2d2c31bb21659a63be1f09f2ee1
WhiteMind <WhiteMind@qq.com>
2022-11-25 20:14:26 +0800

    feat(electron): 实现单例运行限制

--- packages/electron/src/index.ts
+++ packages/electron/src/index.ts
@@ -6,16 +6,58 @@ import { getSettings, setSettings } from './settings'
 import trayPNG from './assets/tray.png'
 import trayICO from './assets/tray.ico'
 
-startServer({
-  getSettings: async () => getSettings(),
-  setSettings: async (newSettings) => setSettings(newSettings),
-  // electron 在 asar 模式下对于一些文件相关的 api 有限制，而 fluent-ffmpeg 使用的 spawn
-  // 则在限制范围中，所以需要解包后的路径（解包是由 electron-builder 内部 hard code 实现的）。
-  // https://www.electronjs.org/fr/docs/latest/tutorial/asar-archives#executing-binaries-inside-asar-archive
-  // https://github.com/electron-userland/electron-builder/blob/6f630927ca949d8bdcde06e4eafaa63ce3636d5a/packages/app-builder-lib/src/asar/unpackDetector.ts#L83
-  // TODO: 更好的方案应该是在这里手动解包，可以确保路径一定正确，这里先简单实现顶着了。
-  ffmpegPath: ffmpegPathFromModule.replace('.asar', '.asar.unpacked'),
+initSingleInstanceLock()
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(initApp)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (
+    process.platform !== 'darwin' &&
+    !getSettings().notExitOnAllWindowsClosed
+  ) {
+    app.quit()
+  }
 })
+
+function initSingleInstanceLock() {
+  const lockSuccess = app.requestSingleInstanceLock()
+
+  if (!lockSuccess) {
+    app.quit()
+    return
+  }
+
+  // TODO: 这个事件处理或许应该放在 initApp 中？
+  app.on('second-instance', showWindow)
+}
+
+function initApp() {
+  startServer({
+    getSettings: async () => getSettings(),
+    setSettings: async (newSettings) => setSettings(newSettings),
+    // electron 在 asar 模式下对于一些文件相关的 api 有限制，而 fluent-ffmpeg 使用的 spawn
+    // 则在限制范围中，所以需要解包后的路径（解包是由 electron-builder 内部 hard code 实现的）。
+    // https://www.electronjs.org/fr/docs/latest/tutorial/asar-archives#executing-binaries-inside-asar-archive
+    // https://github.com/electron-userland/electron-builder/blob/6f630927ca949d8bdcde06e4eafaa63ce3636d5a/packages/app-builder-lib/src/asar/unpackDetector.ts#L83
+    // TODO: 更好的方案应该是在这里手动解包，可以确保路径一定正确，这里先简单实现顶着了。
+    ffmpegPath: ffmpegPathFromModule.replace('.asar', '.asar.unpacked'),
+  })
+
+  createTray()
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+}
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -35,31 +77,6 @@ function createWindow() {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createTray()
-  createWindow()
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (
-    process.platform !== 'darwin' &&
-    !getSettings().notExitOnAllWindowsClosed
-  ) {
-    app.quit()
-  }
-})
 
 function createTray() {
   const tray = new Tray(getTrayImagePath())
@@ -82,7 +99,9 @@ function showWindow() {
   if (windows.length === 0) {
     createWindow()
   } else {
-    windows[0].show()
+    const window = windows[0]
+    if (window.isMinimized()) window.restore()
+    window.show()
   }
 }
 
